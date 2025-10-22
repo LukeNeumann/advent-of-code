32186613ce40de1dcbbba06b2b1318620e828d64
Luke Neumann <lfn1112@gmail.com>
2025-10-22 11:03:12 -0600

    day04 working but slow

--- 2015/day04/root.zig
+++ 2015/day04/root.zig
@@ -4,12 +4,39 @@ const input = @embedFile("input.txt");
 pub fn solve() !void {
     var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
     const alloc = gpa.allocator();
-    defer _ = gpa.deinit(); 
-    _ = alloc;
 
     var it = std.mem.tokenizeScalar(u8, input, '\n');
+    var part1: ?u64 = null;
+    var part2: ?u64 = null;
+    var out: [std.crypto.hash.Md5.digest_length]u8 = undefined;
+
+    var i: u64 = 0;
     while (it.next()) |line| {
-        _ = line;
+        while (true) : (i += 1) {
+            var base_md5 = std.crypto.hash.Md5.init(.{});
+            base_md5.update(line);
+
+            const data = try std.fmt.allocPrint(alloc, "{d}", .{i});
+
+            var md5 = base_md5;
+            md5.update(data);
+            md5.final(&out);
+            const part2_match = std.mem.startsWith(u8, &out, &[3]u8{ 0, 0, 0 });
+            const part1_match = std.mem.startsWith(u8, &out, &[2]u8{ 0, 0 }) and out[2] < 9;
+
+            //const out_hex = std.fmt.bytesToHex(out, std.fmt.Case.lower);
+            //std.debug.print("{s}", .{out_hex});
+
+            if (part1 == null and part1_match) {
+                part1 = i;
+                std.debug.print("part1: {d}\n", .{part1.?});
+            }
+            if (part2 == null and part2_match) {
+                part2 = i;
+                std.debug.print("part2: {d}\n", .{part2.?});
+            }
+
+            if (part1 != null and part2 != null) break;
+        }
     }
-    std.debug.print("{d}\n{d}\n", .{ 0, 0 });
 }
